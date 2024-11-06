4ee91a0e32a26e96eeb0e57e9a53e521e4ea6d3c
Alexander6Shevtsov <axrsvr@gmail.com>
2024-11-06 17:47:38 +0300

    Work

---
+++ Networking/App/SceneDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  SceneDelegate.swift
+//  Networking
+//
+//  Created by Alexander Shevtsov on 06.11.2024.
+//
+
+import Foundation
---
+++ Networking/Views/UserActionCell.swift
@@ -0,0 +1,12 @@
+//
+//  UserActionCell.swift
+//  Networking
+//
+//  Created by Alexander Shevtsov on 06.11.2024.
+//
+
+import UIKit
+
+class UserActionCell: UICollectionViewCell {
+    
+}
